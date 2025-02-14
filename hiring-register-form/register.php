20966da75840634ce5f2e86953c2fc4fd838ad2c
mr-aman-git <amanazr2003@gmail.com>
2025-02-14 10:01:52 +0530

    Register Page add

--- hiring-register-form/register.php
+++ hiring-register-form/register.php
@@ -8,13 +8,36 @@
 </head>
 <body>
     <div class="main-box">
-        <div class="box">
-            <form>
-                <div class="form-box">
-                    <input type="text" name="name" placeholder="Enter your Name">
-                    <input type="text" name="Quali" placeholder="Qualification">
+        <div class="support-div">
+            <div class="box">
+                
+                <form>
+                <div class="heading">
+                    <h1>Apply for Frontend Developer Post</h1>
                 </div>
-            </form>
+                    <div class="form-box">
+                        <input type="text" name="name" placeholder="Enter your Name">
+                        <input type="text" name="Quali" placeholder="Qualification">
+                    
+                        <input type="number" name="number" placeholder="Enter your Number">
+                        <input type="email" name="email" placeholder="Enter Email">
+                    
+                        <input type="text" name="ref" placeholder="Refrence">
+                        <input type="text" name="job" placeholder="Job Profile">
+                    </div>
+
+                    <div class="btn">
+                        <button name="register">Register</button>
+                    </div>
+
+                </form>
+            </div>
+
+            <div class="box2">
+                <h1>Welcome</h1>
+                <p>Please fill all the details carefully. This form can change your life.</p>
+                <button name="check" id="chechbtn">Check Form</button>
+            </div>
         </div>
     </div>
     
